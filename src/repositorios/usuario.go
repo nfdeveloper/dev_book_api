1da70ae6bac30e34c125307633469d32180fc31a
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-01 14:47:40 -0300

    feat: delete usuarios

--- src/repositorios/usuario.go
+++ src/repositorios/usuario.go
@@ -118,3 +118,18 @@ func (repositorio Usuarios) Atualizar(id uint64, usuario modelos.Usuario) error 
 	return nil
 
 }
+
+func (repositorio Usuarios) Deletar(id uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from usuarios where id = ?")
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro = statement.Exec(id); erro != nil {
+		return erro
+	}
+
+	return nil
+}
