4c6608fac4f917cf17e4cfaa8e4fc57426cd342c
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-01 13:19:03 -0300

    feat: implementando métodos de validação de usuarios.

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,10 +1,51 @@
 package modelos
 
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
 type Usuario struct {
-	ID       uint64 `json:"id,omitempty"`
-	Nome     string `json:"nome,omitempty`
-	Nick     string `json:"nick,omitempty`
-	Email    string `json:"email,omitempty`
-	Senha    string `json:"senha,omitempty`
-	CriadoEm string `json:"criado_em,omitempty`
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty`
+	Nick     string    `json:"nick,omitempty`
+	Email    string    `json:"email,omitempty`
+	Senha    string    `json:"senha,omitempty`
+	CriadoEm time.Time `json:"criado_em,omitempty`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco.")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nome de Usuário é obrigatório e não pode estar em branco.")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em branco.")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar em branco.")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
