b368ef21e3b92a4d93b0e17a87ba09f3845f57ae
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-02 12:47:44 -0300

    feat: criacao de publicacao

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -1,8 +1,55 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-func CriarPublicacao(w http.ResponseWriter, r *http.Request)     {}
+	"github.com/nfdeveloper/dev_book_api/src/autenticacao"
+	"github.com/nfdeveloper/dev_book_api/src/banco"
+	"github.com/nfdeveloper/dev_book_api/src/modelos"
+	"github.com/nfdeveloper/dev_book_api/src/repositorios"
+	"github.com/nfdeveloper/dev_book_api/src/respostas"
+)
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioId(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorId = usuarioID
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
+
+}
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request)   {}
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request)    {}
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {}
